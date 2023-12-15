16281c964298e8d2e9015113f46e3c5fdd3f7c42
Diego Aguilera <aguileraGit@users.noreply.github.com>
2023-12-14 21:16:57 -0500

    Fixed ADC error.

--- main.ts
+++ main.ts
@@ -7,11 +7,10 @@ input.onButtonPressed(Button.A, function () {
     dir = !(dir)
 })
 input.onButtonPressed(Button.B, function () {
-    speed += speed + 1
+    speed = speed + 1
     if (speed > 10) {
         speed = 0
     }
-    pins.analogWritePin(AnalogPin.P2, 2 ** speed)
 })
 function rotateCC () {
     pins.digitalWritePin(DigitalPin.P0, 1)
@@ -19,15 +18,15 @@ function rotateCC () {
 }
 function rotateCCW () {
     pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 1)
 }
 let speed = 0
 let dir = false
-pins.digitalWritePin(DigitalPin.P0, 1)
+pins.digitalWritePin(DigitalPin.P0, 0)
 pins.digitalWritePin(DigitalPin.P1, 0)
-pins.analogWritePin(AnalogPin.P2, 0)
+pins.analogWritePin(AnalogPin.P2, 1023)
 dir = false
-speed = 5
+speed = 0
 basic.forever(function () {
-	
+    pins.analogWritePin(AnalogPin.P2, 2 ** speed - 1)
 })
